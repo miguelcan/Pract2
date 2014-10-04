ec00b3e128ad4acfc58092b2d79e9b7046a12b7b
mike <mike@Mac-Pro-de-mike.local>
2014-10-04 15:25:34 -0700

    Initial Commit

---
+++ Prac2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Prac2
+//
+//  Created by mike on 04/10/14.
+//  Copyright (c) 2014 mike. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Prac2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Prac2
+//
+//  Created by mike on 04/10/14.
+//  Copyright (c) 2014 mike. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
